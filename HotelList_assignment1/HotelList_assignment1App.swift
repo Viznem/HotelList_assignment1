ddcf4ab4fa557b239277c940718de7c2dd555f4c
Viznem <thinhtruong3344@gmail.com>
2022-07-27 11:55:51 +0700

    Initial Commit

---
+++ HotelList_assignment1/HotelList_assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  HotelList_assignment1App.swift
+//  HotelList_assignment1
+//
+//  Created by Thinh, Nguyen Truong on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotelList_assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
